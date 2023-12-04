c8c6a12ef18491c821f033631be88c369e043a2f
rakshith_s <rakshithsantosh@gmail.com>
2023-12-04 07:51:54 +0530

    tailwind

---
+++ projects/tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './components/card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline mb-6">
+      Hello world!
+    </h1>
+    
+    <Card username='someuser'/>
+    
+    </>
+  )
+}
+
+export default App
